4fea4001e248ca37159892f66757911919d8904e
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-05-16 00:51:27 +0200

    GlobalStyle,ThemeProvider

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,::after,::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: "Inter", sans-serif;
+    font-size: 18px;
+    margin: 0 auto;
+    padding-bottom: 108px;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './core/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/theme";
+import { GlobalStyle } from "./core/GlobalStyle";
+import App from "./core/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
