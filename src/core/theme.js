4ba3590b53211d7197ce4edb682ba10af220dce8
Urszula Malinowska <ula.malin35@gmail.com>
2023-05-17 22:56:20 +0200

    breakpoints and colors added in theme.js

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,7 +1,13 @@
 export const theme = {
-    colors: {
-    },
-    
-    breakpoints: {
-    },
-  };
+  colors: {
+    white: "#FFFFFF",
+    mystic: "#E4E6F0",
+    cornflowerBlue: "#7E839A",
+  },
+
+  breakpoints: {
+    mobileMax: 767,
+    mobileMin:460,
+    full:1344,
+  },
+};
