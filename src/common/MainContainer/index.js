8b7ad220c81361c344104ef2af6a362ea9093eba
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-05-19 21:34:52 +0200

    paste PopularMoviesPage to MainContainer

--- src/common/MainContainer/index.js
+++ src/common/MainContainer/index.js
@@ -1,24 +1,7 @@
-import { Container, TilesContainer, Header } from "./styled";
-import { Tile } from "../Tile";
-import { sampleTiles } from "./sampleTiles";
+import { PopularMoviesPage } from "../../features/PopularMoviesPage";
 
 export const MainContainer = () => {
   return (
-    <Container>
-      <Header>Popular movies</Header>
-      <TilesContainer>
-        {sampleTiles.map((tile) => (
-          <Tile
-            poster={tile.poster}
-            tileTitle={tile.tileTitle}
-            tileSubtitle={tile.tileSubtitle}
-            genres={tile.genres}
-            rate={tile.rate}
-            votesNr={tile.votesNr}
-            key={tile.id}
-          ></Tile>
-        ))}
-      </TilesContainer>
-    </Container>
+   <PopularMoviesPage/>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./core/theme";
 import { GlobalStyle } from "./core/GlobalStyle";
 import reportWebVitals from "./reportWebVitals";
-import MovieData from "./MovieData"; 
+import { MainContainer } from "./common/MainContainer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <MovieData />
+      <MainContainer />
     </ThemeProvider>
  </React.StrictMode>
 );
