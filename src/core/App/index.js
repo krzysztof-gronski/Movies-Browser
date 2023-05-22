6249fd86583683d3f36b2fe56d292a639ec2575e
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-05-22 22:27:30 +0200

    test

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -1,21 +1,18 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Navigation from "../../common/Navigation";
 import { PopularMoviesPage } from "../../features/PopularMoviesPage";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="https://krzysztof-gronski.github.io/Movies-Browser/">
+      <HashRouter>
         <Navigation />
         <Switch>
-          <Route path="/people/:id">{"People Details"}</Route>
-          <Route path="/people/:id">{"Movie"}</Route>
-          <Route path="/people">{"People"}</Route>
           <Route path="/">
             <PopularMoviesPage />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
