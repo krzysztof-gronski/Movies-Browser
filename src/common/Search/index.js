3a007a17b0686381fadf3ee7e5ddd0658a1ae14c
Urszula Malinowska <ula.malin35@gmail.com>
2023-05-23 18:41:38 +0200

    query seen as value in input, without onChange yet

--- src/common/Search/index.js
+++ src/common/Search/index.js
@@ -1,5 +1,16 @@
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
 import { Input } from "./styled";
 
-const Search = () => <Input placeholder={`Search for movies...`} />;
+const Search = () => {
+  const location = useLocation()
+  const query = (new URLSearchParams(location.search)).get("search");
+
+  return (
+    <Input 
+  value={query || ""}
+  placeholder={`Search for movies...`} 
+  />
+  )
+};
 
 export default Search;
