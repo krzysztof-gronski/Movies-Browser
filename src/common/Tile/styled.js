9b4a6ae46b59f1fcac12e5ef2be95f848fee6585
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-05-23 15:11:26 +0200

    tile styling update

--- src/common/Tile/styled.js
+++ src/common/Tile/styled.js
@@ -5,16 +5,34 @@ export const TileContainer = styled.div`
   grid-template-rows: auto 1fr;
   justify-items: center;
   width: 324px;
-  height: 650px;
+  height: 100%; //650px;
   background-color: ${({ theme }) => theme.colors.white};
   padding: 16px;
   box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
   border-radius: 5px;
   transition: transform 0.3s linear;
+  border: 1px solid gray;
 
   &:hover {
     cursor: pointer;
     transform: scale(1.02);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.smallScreen}px) {
+    width: 100%;
+    min-width: 324px;
+    height: 100%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    grid-template-columns: auto 1fr;
+    //width: 100%;
+    //height: auto;
+    aspect-ratio: 1.432;
+    min-width: 288px;
+    min-height: 201px;
+    grid-gap: 16px;
+    padding-bottom: 0px;
   }
 `;
 
@@ -22,15 +40,41 @@ export const Poster = styled.img`
   width: 292px;
   height: 434px;
   margin-left: -1px;
+  border-radius: 5px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.smallScreen}px) {
+    width: 100%;
+    height: auto;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    //width: 40%;
+    //min-width: 114px;
+    width: 100%;
+    height: 100%;
+    min-height: 169px;
+    //height: auto;
+  }
 `;
 
 export const TileContent = styled.div`
   display: flex;
   flex-direction: column;
   justify-self: start;
-  margin: 16px 0px 16px -1px;
-  height: 168px;
+  margin: 16px 0px 5px -1px;
+  //height: 168px;
   width: 292px;
+  border: 1px solid green;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.smallScreen}px) {
+    width: 100%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    margin: 0px;
+    width: 100%;
+    min-width: 126px;
+  }
 `;
 
 export const TileTitle = styled.header`
@@ -38,21 +82,36 @@ export const TileTitle = styled.header`
   font-weight: 500;
   font-size: 22px;
   line-height: 1.3;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    margin-bottom: 4px;
+    font-size: 16px;
+  }
 `;
 
 export const TileSubtitle = styled.div`
   font-weight: 400;
   font-size: 16px;
   color: ${({ theme }) => theme.colors.waterloo};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    font-size: 13px;
+  }
 `;
 
 export const TileGenres = styled.ul`
   display: flex;
   flex-wrap: wrap;
   flex-grow: 1;
-  margin: 5px 0px 0px;
+  margin: 8px 0px 0px;
   padding-left: 0;
   list-style: none;
+  border: 1px solid blue;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    //margin: 0px 0px 0px 0px;
+    flex-grow: 0;
+  }
 `;
 
 export const TileGenre = styled.li`
@@ -63,6 +122,14 @@ export const TileGenre = styled.li`
   font-size: 14px;
   line-height: 1.4;
   border-radius: 5px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    //margin: 0px 0px 0px 0px;
+    padding: 4px 8px;
+    height: 19px;
+    font-size: 10px;
+    line-height: 1.1;
+  }
 `;
 
 export const TileDescription = styled.p`
@@ -76,33 +143,59 @@ export const TileDescription = styled.p`
 export const RateContainer = styled.div`
   display: flex;
   justify-self: flex-end;
+  border: 1px solid red;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    margin-top: -5px;
+  }
 `;
 
-export const StarIcon = styled.img``;
+export const StarIcon = styled.img`
+  width: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    margin-top: 1px;
+    width: 16px;
+  }
+`;
 
 export const Rate = styled.span`
   margin-left: 12px;
-  margin-top: 2px;  
+  margin-top: 2px;
   color: ${({ theme }) => theme.colors.woodsmoke};
   font-weight: 600;
   font-size: 16px;
   line-height: 1.5;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    margin-left: 8px;
+    font-size: 13px;
+  }
 `;
 
 export const VotesNr = styled.span`
   margin-left: 12px;
-  margin-top: 2px;  
+  margin-top: 2px;
   color: ${({ theme }) => theme.colors.waterloo};
   font-weight: 400;
   font-size: 16px;
   line-height: 1.5;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    margin-left: 7px;
+    font-size: 13px;
+  }
 `;
 
 export const VotesLabel = styled.span`
   margin-left: 5px;
-  margin-top: 2px;  
+  margin-top: 2px;
   color: ${({ theme }) => theme.colors.waterloo};
   font-weight: 400;
   font-size: 16px;
   line-height: 1.5;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    font-size: 13px;
+  }
 `;
