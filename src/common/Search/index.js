2f99767f90ef6fb789ec36c96e69897acf188d61
Urszula Malinowska <ula.malin35@gmail.com>
2023-05-23 19:30:12 +0200

    onInputChange function implemented in code

--- src/common/Search/index.js
+++ src/common/Search/index.js
@@ -2,15 +2,29 @@ import { useLocation, useHistory } from "react-router-dom";
 import { Input } from "./styled";
 
 const Search = () => {
-  const location = useLocation()
-  const query = (new URLSearchParams(location.search)).get("search");
+  const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get("search");
+
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+    if (target.value === "") {
+      searchParams.delete("search");
+    } else {
+      searchParams.set("search", target.value);
+    }
+    const newSearch = searchParams.toString();
+
+    return history.push(`${location.pathname}?${newSearch}`);
+  };
 
   return (
-    <Input 
-  value={query || ""}
-  placeholder={`Search for movies...`} 
-  />
-  )
+    <Input
+      onChange={onInputChange}
+      value={query || ""}
+      placeholder={`Search for movies...`}
+    />
+  );
 };
 
 export default Search;
