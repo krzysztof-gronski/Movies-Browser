562256acd77cc74ddb1f3359118505b807e2f94a
Urszula Malinowska <ula.malin35@gmail.com>
2023-05-27 00:04:56 +0200

    changes of styles in pagination styled.js

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -1,5 +1,4 @@
-import styled, { css } from "styled-components";
-import { ReactComponent as Icon } from "../../images/Vector.svg";
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   width: 100%;
@@ -7,12 +6,13 @@ export const Wrapper = styled.div`
   justify-content: center;
   align-items: flex-end;
   margin-bottom: 104px;
+  margin-top: 40px;
 `;
 
-export const StyledButton = styled.button`
+export const Button = styled.button`
   background: #d6e4ff;
   min-width: 76px;
-  max-width: 106px;
+  max-width: 108px;
   height: 36px;
   border-radius: 5px;
   white-space: nowrap;
@@ -22,25 +22,28 @@ export const StyledButton = styled.button`
   line-height: 140%;
   margin: 0 12px;
   color: ${({ theme }) => theme.colors.black};
-  background-color: ${({ theme }) => theme.colors.mystic};
+  background-color: ${({ theme }) => theme.colors.pattensBlue};
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
   border: none;
   &:disabled {
-    background: #e5e5e5;
+    background-color: ${({ theme }) => theme.colors.mystic};
+  }
+  @media (max-width: 767px) {
+    height: 24px;
+    max-width: 38px;
+    min-width: 29px;
+    padding: 8px 6px;
   }
 `;
 
-export const StyledIcon = styled(Icon)`
-  margin-right: 8px;
-  ${({ rotate }) =>
-    rotate &&
-    css`
-      rotate: 180deg;
-      margin-left: 8px;
-      margin-right: 0;
-    `}
+export const ButtonText = styled.span`
+  @media (max-width: 767px) {
+    display: none;
+  }
 `;
-
-export const ButtonText = styled.span``;
 
 export const PageIndicator = styled.div`
   padding-top: 8px;
@@ -49,9 +52,24 @@ export const PageIndicator = styled.div`
   line-height: 24px;
   margin-left: 24px;
   margin-right: 24px;
+  @media (max-width: 767px) {
+    font-size: 10px;
+    margin-left: 8px;
+    margin-right: 8px;
+    padding: 0;
+  }
 `;
 export const PageNumber = styled.span`
   font-weight: 600;
   font-size: 16px;
   color: ${({ theme }) => theme.colors.black};
+  @media (max-width: 767px) {
+    font-size: 10px;
+  }
+`;
+
+export const VectorSvg = styled.svg`
+  width: 8px;
+  height: 11px;
+  fill: none;
 `;
