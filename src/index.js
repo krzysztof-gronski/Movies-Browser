40e9ece63f078a2efab8cb74710b2b71dde8a470
Urszula Malinowska <ula.malin35@gmail.com>
2023-05-28 13:21:14 +0200

    accessing the store

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ import { theme } from "./core/theme";
 import { GlobalStyle } from "./core/GlobalStyle";
 import reportWebVitals from "./reportWebVitals";
 import App from "./core/App";
+import store from "./core/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
- </React.StrictMode>
+  </React.StrictMode>
 );
 
 reportWebVitals();
