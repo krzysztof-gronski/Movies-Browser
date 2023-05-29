0ca78cc174d917e0f0712313881c59b774e8e4b2
Urszula Malinowska <ula.malin35@gmail.com>
2023-05-29 21:24:07 +0200

    ErrorPage Loader Noresults MovieDetails moved to seperate components

--- src/common/ErrorPage/index.js
+++ src/common/ErrorPage/index.js
@@ -1,5 +1,14 @@
-import { MainContainer } from "../MainContainer";
+import { Container, Header, Message } from "../MainContainer/styled";
+import { ErrorImage, LinkButton } from "./styled";
 
 export const ErrorPage = () => {
-  return <MainContainer error/>;
+  return (
+    <Container error>
+      <ErrorImage src={ErrorImage} />
+      <Header error>{"Ooops! Something went wrong..."}</Header>{" "}
+      <Message>Please check your network connection</Message>
+      <Message>and try again</Message>
+      <LinkButton to="/">Back to home page</LinkButton>
+    </Container>
+  );
 };
