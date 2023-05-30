34131efed1208bf32995c1d5661bf1e690508a3c
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-05-30 12:03:52 +0200

    console deleted

--- src/features/MoviesList/getMovies.js
+++ src/features/MoviesList/getMovies.js
@@ -3,7 +3,6 @@ import { tmdbApi } from "../api/apiData";
 export const getMovies = async (page) => {
   try {
     const response = await tmdbApi.getPopularMoviesData(page);
-    console.log(response.data);
 
     if (!response || !response.data) {
       throw new Error("Invalid response data");
