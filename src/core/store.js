e0d7bff997d80952e7beb991fbf7087d58850874
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-05-31 01:26:05 +0200

    movieDetailsReducer added to store

--- src/core/store.js
+++ src/core/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 import moviesReducer from "../features/MoviesList/moviesListSlice";
+import movieDetailsReducer from "../features/MovieDetails/movieDetailsSlice"
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    movieDetails: movieDetailsReducer,
   },
   middleware: [sagaMiddleware],
 });
