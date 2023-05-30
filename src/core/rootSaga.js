ebb37dd9b4b05a41b99710dcf3ed7de39f80b6b3
Urszula Malinowska <ula.malin35@gmail.com>
2023-05-30 22:20:02 +0200

    peopleListSlice reducer conntected to the store and peopleListSaga added to rootSaga.js

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import { moviesListSaga } from "../features/MoviesList/moviesListSaga";
+import { peopleListSaga } from "../features/PeopleList/peopleListSaga";
 
 export default function* rootSaga() {
   yield all([
     moviesListSaga(),
+    peopleListSaga(),
 ]);
 }
--- src/core/store.js
+++ src/core/store.js
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 import moviesReducer from "../features/MoviesList/moviesListSlice";
-
+import peopleReducer from "../features/PeopleList/peopleListSlice";
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    people: peopleReducer,
   },
   middleware: [sagaMiddleware],
 });
