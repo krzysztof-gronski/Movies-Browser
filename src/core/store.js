617e1ee0e87968b21826976ca7ec2153ece3ddf9
Ulyana Lakevich <ulyanalakevich@gmail.com>
2023-05-31 11:42:24 +0200

    personDetails and peopleList added to store

--- src/core/store.js
+++ src/core/store.js
@@ -3,6 +3,8 @@ import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 import moviesReducer from "../features/MoviesList/moviesListSlice";
 import movieDetailsReducer from "../features/MovieDetails/movieDetailsSlice"
+import peopleListReducer from "../features/PeopleList/peopleListSlice"
+import personDetailsReducer from "../features/PersonDetails/personDetailsSlice"
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -10,6 +12,8 @@ const store = configureStore({
   reducer: {
     movies: moviesReducer,
     movieDetails: movieDetailsReducer,
+    peopleList: peopleListReducer,
+    personDetails: personDetailsReducer,
   },
   middleware: [sagaMiddleware],
 });
