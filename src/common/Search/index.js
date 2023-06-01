bb5d4cf8c479ab9e0af0fba51365b5614a823ff5
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-06-01 02:22:20 +0200

    search trigger on input event

--- src/common/Search/index.js
+++ src/common/Search/index.js
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import { Input } from "./styled";
 import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
 import searchQueryParamName from "./searchQueryParamName";
