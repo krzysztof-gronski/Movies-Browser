e02c55736f678fff744b86a833e73d346d6ba6d1
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2023-06-03 20:30:55 +0200

    empty backdrop bug fixed

--- src/features/MovieDetails/index.js
+++ src/features/MovieDetails/index.js
@@ -26,56 +26,60 @@ export const MovieDetails = () => {
     dispatch(fetchMovieDetails({ movieId: id }));
   }, [id, dispatch]);
 
+  console.log(movieDetails);
+
   return (
     movieDetails && (
       <>
-        <Backdrop
-          backdropImage={
-            movieDetails.backdrop_path
-              ? `${IMAGE_PATH}${movieDetails.backdrop_path}`
-              : null
-          }
-          movie={movieDetails}
-        ></Backdrop>
+        {movieDetails.backdrop_path ? (
+          <Backdrop
+            backdropImage={
+              movieDetails.backdrop_path
+                ? `${IMAGE_PATH}${movieDetails.backdrop_path}`
+                : null
+            }
+            movie={movieDetails}
+          ></Backdrop>
+        ) : null}
         <Container movieDetailsFlag>
           <Tile
-          movieDetailsFlag
-          key={movieDetails.id}
-          movie={movieDetails}
-          poster={
-            movieDetails.poster_path
-              ? `${IMAGE_PATH}${movieDetails.poster_path}`
-              : null
-          }
-          tileTitle={movieDetails.original_title}
-          tileSubtitle={
-            movieDetails.release_date
-              ? movieDetails.release_date.slice(0, 4)
-              : ""
-          }
-          production={
-            movieDetails.production_countries
-              ? movieDetails.production_countries.length > 0
-                ? movieDetails.production_countries
-                    .map((productionObj) => productionObj.name)
-                    .join(", ")
+            movieDetailsFlag
+            key={movieDetails.id}
+            movie={movieDetails}
+            poster={
+              movieDetails.poster_path
+                ? `${IMAGE_PATH}${movieDetails.poster_path}`
+                : null
+            }
+            tileTitle={movieDetails.original_title}
+            tileSubtitle={
+              movieDetails.release_date
+                ? movieDetails.release_date.slice(0, 4)
                 : ""
-              : ""
-          }
-          releaseDate={
-            movieDetails.release_date
-              ? formatDate(movieDetails.release_date)
-              : ""
-          }
-          genres={
-            movieDetails.genres
-              ? movieDetails.genres.map((genre) => genre.name)
-              : [""]
-          }
-          rate={movieDetails.vote_average}
-          votesNr={movieDetails.vote_count}
-          description={movieDetails.overview}
-        ></Tile>
+            }
+            production={
+              movieDetails.production_countries
+                ? movieDetails.production_countries.length > 0
+                  ? movieDetails.production_countries
+                      .map((productionObj) => productionObj.name)
+                      .join(", ")
+                  : ""
+                : ""
+            }
+            releaseDate={
+              movieDetails.release_date
+                ? formatDate(movieDetails.release_date)
+                : ""
+            }
+            genres={
+              movieDetails.genres
+                ? movieDetails.genres.map((genre) => genre.name)
+                : [""]
+            }
+            rate={movieDetails.vote_average}
+            votesNr={movieDetails.vote_count}
+            description={movieDetails.overview}
+          ></Tile>
         </Container>
       </>
     )
