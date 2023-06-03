54f9b382b947c4240a4655bd753e27026856d0a8
Urszula Malinowska <ula.malin35@gmail.com>
2023-06-03 15:08:40 +0200

    PeopleList component full functioning, data from api

--- src/features/PeopleList/index.js
+++ src/features/PeopleList/index.js
@@ -1,30 +1,68 @@
-
-import { Container, Header, TilesContainer } from "../../common/MainContainer/styled";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Container,
+  Header,
+  TilesContainer,
+} from "../../common/MainContainer/styled";
 import { Tile } from "../../common/Tile";
 import missingPersonPoster from "../../images/missingPersonPoster.svg";
 import { IMAGE_PATH } from "../api/apiData";
-import { samplePeople } from "./samplePeople";
+import {
+  fetchPeople,
+  selectPeople,
+  selectStatus,
+  selectTotalPages,
+  setQuery,
+} from "./peopleListSlice";
+import { useEffect } from "react";
+import { useQueryParameter } from "../../common/Search/queryParameters";
+import Pagination from "../../common/Pagination";
+import { Loader } from "../../common/Loader";
+import { ErrorPage } from "../../common/ErrorPage";
+import { NoResults } from "../../common/NoResults";
+import { StyledLink } from "./styled";
 
-export const PeopleList = () => { 
-    
-  return (
+export const PeopleList = () => {
+  const dispatch = useDispatch();
+  let status = useSelector(selectStatus);
+  const people = useSelector(selectPeople);
+  const totalPages = useSelector(selectTotalPages);
+  let page = useQueryParameter("page");
+  const query = useQueryParameter("search");
+
+  if (!page) page = 1;
+
+  useEffect(() => {
+    dispatch(setQuery(query ? { query: query } : { query: "" }));
+    dispatch(fetchPeople(page));
+  }, [page]);
+
+  return status === "loading" ? (
+    <Loader />
+  ) : status === "error" ? (
+    <ErrorPage />
+  ) : query && people.length <= 0 ? (
+    <NoResults />
+  ) : (
     <Container peopleListFlag>
       <Header>Popular people</Header>
       <TilesContainer peopleListFlag>
-        {samplePeople.map((person) => (
-          <Tile
-            peopleListFlag
-            key={person.id}
-            poster={
-              person.profile_path
-                ? `${IMAGE_PATH}${person.profile_path}`
-                : `${missingPersonPoster}`
-            }
-            tileTitle={person.name ? person.name : ""}
-          ></Tile>
+        {people.map((person) => (
+          <StyledLink to={`/person/${person.id}`}>
+            <Tile
+              peopleListFlag
+              key={person.id}
+              poster={
+                person.profile_path
+                  ? `${IMAGE_PATH}${person.profile_path}`
+                  : `${missingPersonPoster}`
+              }
+              tileTitle={person.name ? person.name : ""}
+            ></Tile>
+          </StyledLink>
         ))}
       </TilesContainer>
-      {/* <Pagination page={page} totalPages={totalPages} /> */}
+      <Pagination page={page} totalPages={totalPages} />
     </Container>
   );
 };
