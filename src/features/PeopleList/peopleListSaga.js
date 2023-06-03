37569ba2e401c3f86bc127f5bcadd052702d09e7
Urszula Malinowska <ula.malin35@gmail.com>
2023-06-03 15:07:57 +0200

    delay added, page  as second  argument

--- src/features/PeopleList/peopleListSaga.js
+++ src/features/PeopleList/peopleListSaga.js
@@ -1,21 +1,30 @@
-import { call, put, debounce, select } from "redux-saga/effects";
+import {
+  call,
+  put,
+  debounce,
+  select,
+  takeLatest,
+  delay,
+} from "redux-saga/effects";
 import {
   selectPage,
   selectQuery,
   fetchPeople,
   fetchPeopleError,
+  fetchPeopleSuccess,
 } from "./peopleListSlice";
 import { getPeople, searchPerson } from "../api/apiData";
 
 function* fetchPeopleHandler() {
   try {
+    yield delay(1000);
     const page = yield select(selectPage);
     const query = yield select(selectQuery);
     let people;
     if (query !== "") {
-      people = yield call(searchPerson, query);
+      people = yield call(searchPerson, query, page);
     } else {
-      people = yield call(getPeople,  page );
+      people = yield call(getPeople, page);
     }
     yield put(fetchPeopleSuccess(people));
   } catch (error) {
@@ -24,5 +33,5 @@ function* fetchPeopleHandler() {
 }
 
 export function* peopleListSaga() {
-    yield takeLatest(fetchPeople.type, fetchPeopleHandler);
+  yield takeLatest(fetchPeople.type, fetchPeopleHandler);
 }
