eb43ba0e194790502b8fd09816d3b9d81a2785c8
Urszula Malinowska <ula.malin35@gmail.com>
2023-06-05 17:50:09 +0200

    PersonDetails comopnent updated, full functioning, connected with api

--- src/features/PersonDetails/index.js
+++ src/features/PersonDetails/index.js
@@ -4,6 +4,8 @@ import { Tile } from "../../common/Tile";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getDetailsForPerson, selectDetails } from "./personDetailsSlice";
+import { IMAGE_PATH } from "../api/apiData";
+import missingPersonPoster from "../../images/missingPersonPoster.svg";
 
 export const PersonDetails = () => {
   const dispatch = useDispatch();
@@ -17,15 +19,28 @@ export const PersonDetails = () => {
 
   console.log(personDetails);
 
-  let imgPath = "https://image.tmdb.org/t/p/w500"+personDetails.profile_path;
-
   return (
+    personDetails && (
       <Container personDetailsFlag>
-        <div>{`${personDetails.name}`}</div>
-        <div>{`${personDetails.birthday}`}</div>
-        <div>{`${personDetails.place_of_birth}`}</div>
-        <div>{`${personDetails.biography}`}</div>
-        <img src={imgPath}/>
+        <Tile
+          personDetailsFlag
+          key={personDetails.id}
+          person={personDetails}
+          poster={
+            personDetails.profile_path
+              ? `${IMAGE_PATH}${personDetails.profile_path}`
+              : `${missingPersonPoster}`
+          }
+          tileTitle={personDetails.name}
+          tileSubtitle={
+            personDetails.birthday ? personDetails.birthday.slice(0, 4) : ""
+          }
+          production={
+            personDetails.place_of_birth ? personDetails.place_of_birth : ""
+          }
+          description={personDetails.biography}
+        ></Tile>
       </Container>
+    )
   );
 };
