366b6dc41bc315843da5cf65446f34cfe9224ff9
Urszula Malinowska <ula.malin35@gmail.com>
2023-06-05 17:52:57 +0200

    peopleListSaga added to rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -2,7 +2,13 @@ import { all } from "redux-saga/effects";
 import { moviesListSaga } from "../features/MoviesList/moviesListSaga";
 import { movieDetailsSaga } from "../features/MovieDetails/movieDetailsSaga";
 import { personDetailsSaga } from "../features/PersonDetails/personDetailsSaga";
+import { peopleListSaga } from "../features/PeopleList/peopleListSaga";
 
 export default function* rootSaga() {
-  yield all([moviesListSaga(), movieDetailsSaga(), personDetailsSaga()]);
+  yield all([
+    moviesListSaga(),
+    movieDetailsSaga(),
+    peopleListSaga(),
+    personDetailsSaga(),
+  ]);
 }
